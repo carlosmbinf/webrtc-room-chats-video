d718c5bf82f74f5eef7ec1b2cde53493be0bc403
Carlos Medina <carlosmbinf@gmail.com>
2021-07-18 23:11:49 -0400

    change to http

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ const app = express(),
     cert: fs.readFileSync(__dirname + '/rtc-video-room-cert.pem')
   },
   port = process.env.PORT || 3005,
-  server = process.env.NODE_ENV === 'production' ?
-    http.createServer(app).listen(port) :
-    https.createServer(options, app).listen(port),
+  server = 
+  // process.env.NODE_ENV === 'production' ?
+    http.createServer(app).listen(port) 
+    // :
+    // https.createServer(options, app).listen(port),
   io = sio(server);
 // compress all requests
 app.use(compression());
